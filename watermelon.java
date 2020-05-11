7d545456f76fe544090440942dc8a02ad3fd3874
tenos200 <tim.enosedu@gmail.com>
2020-05-11 12:14:04 +0200

    first commit

---
+++ watermelon.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class watermelon{
+
+
+
+	static Scanner in = new Scanner(System.in);
+	public static void main(String[]args){
+		
+		int n = in.nextInt();
+		
+
+		if(n != 2 && n % 2 == 0){
+			System.out.println("YES");
+
+		
+		}
+		else{
+			System.out.println("NO");
+
+		
+		}
+	
+	
+	}
+
+
+}
