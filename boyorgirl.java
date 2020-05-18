222ef541c26d54f7f84926b743efe6a5278a2985
tenos200 <tim.enosedu@gmail.com>
2020-05-18 13:59:13 +0200

    s

---
+++ boyorgirl.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class boyorgirl{
+	static Scanner in = new Scanner(System.in);
+	public static void main(String[]args){
+		
+		int count = 0;
+		int count2 = 0;
+		String s = in.next();
+		char [] name = new char[s.length()];
+		name = s.toCharArray();
+
+		for(int i = 0; i < name.length; i++){
+			for(int j = 0; j < name.length; j++){
+			
+			}
+		}
+		
+
+
+		System.out.println(count);
+
+
+		
+
+		
+	
+	
+	
+	
+	
+	}
+}
