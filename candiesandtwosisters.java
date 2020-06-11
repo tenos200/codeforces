13792b2427371e0822205013fc71304019a2cf2d
tenos200 <tim.enosedu@gmail.com>
2020-06-11 13:37:21 +0200

    1335A, java, python solution neat quick division

---
+++ candiesandtwosisters.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class candiesandtwosisters{
+
+	static Scanner in = new Scanner(System.in);
+
+	public static void main(String[]args){
+	
+		int t = in.nextInt();
+	
+		for(int i = 0; i < t; i++){
+			
+			int n = in.nextInt();
+		
+			System.out.println((n- 1) / 2);
+		}
+
+		
+			
+
+	}
+}
