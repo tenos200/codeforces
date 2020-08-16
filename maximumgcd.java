46896fc61fd4fdc624330a695e096ab3093c48c5
tenos200 <tim.enosedu@gmail.com>
2020-08-16 22:33:49 +0200

    problem 1370A time complexity O(1)

---
+++ maximumgcd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class maximumgcd{
+
+	public static void main(String[]args){
+	
+		Scanner in = new Scanner(System.in);
+
+		int t = in.nextInt();
+
+		while(t-- > 0){
+
+			int n = in.nextInt();
+
+
+			System.out.println(n / 2);
+			
+		}
+	}
+}
