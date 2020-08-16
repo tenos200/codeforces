33242efcc2134aba6d180b6545c444838d17f05f
tenos200 <tim.enosedu@gmail.com>
2020-08-16 11:05:10 +0200

    problem 1371A

---
+++ magicalsticks.py
@@ -0,0 +1,8 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    if n % 2 == 0:
+        print(int(n / 2))
+    else:
+        print(int((n + 1) / 2))
---
+++ problem1097A.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class problem1097A{
+
+	public static void main(String[]args){
+		
+	}
+}
