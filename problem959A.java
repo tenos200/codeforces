13e137d6646be6cd4f9271c51f819ebe1111c2d3
tenos200 <tim.enosedu@gmail.com>
2020-09-16 22:06:24 +0100

    problem959A java and python

---
+++ problem959A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class problem959A {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+
+		if (n % 2 == 0) {
+			System.out.println("Mahmoud");
+			
+		}
+		else{
+
+			System.out.println("Ehab");
+		}
+	}
+}
