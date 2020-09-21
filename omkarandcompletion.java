c92cd0111515b144ea889adda0b5c3812298638f
tenos200 <tim.enosedu@gmail.com>
2020-09-21 21:08:59 +0100

    problem1372A easy solution java, python

---
+++ omkarandcompletion.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class omkarandcompletion {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		
+		int t = in.nextInt();
+
+		for (int i = 0; i < t; i++) {
+
+			int n = in.nextInt();
+
+			for (int j = 0; j < n; j++) {
+				
+				System.out.print("1 ");
+
+			}
+
+			System.out.println();
+			
+		}
+	}
+}
