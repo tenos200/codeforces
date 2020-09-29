651d201daee62f8adb09e772132235389b000fe4
tenos200 <tim.enosedu@gmail.com>
2020-09-29 15:51:16 +0100

    problem1325A, java and python

---
+++ ehabandgcd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ehabandgcd {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int t = in.nextInt();
+
+		while (t -- > 0) {
+
+			int x = in.nextInt();
+
+			System.out.println(1 + " " + (x - 1));
+			
+		}
+	}
+}
